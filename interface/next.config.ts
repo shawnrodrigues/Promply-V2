638c6cdb5ba58a0209ebde421a57202035f9b11f
Shawn Rodrigues <shawn.rodrigues3308@gmail.com>
2025-09-26 23:17:40 +0530

    Add allowedDevOrigins to Next.js config
    Introduces the allowedDevOrigins property to the Next.js configuration, specifying 'http://20.0.0.12:3000' as an allowed development origin.

--- interface/next.config.ts
+++ interface/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Add the allowedDevOrigins property here
+  allowedDevOrigins: [
+    'http://20.0.0.12:3000'
+  ],
+  /* other config options here */
 };
 
 export default nextConfig;
